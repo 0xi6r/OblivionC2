6eb0a3eba0fa39e542b783ac0ea7d33ffa5eefb3
issachar5 <193503195+issachar5@users.noreply.github.com>
2025-04-17 15:28:50 +0000

    Thu, Apr 17, 2025, 6:28 PM +03:00

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use active_web::{web, App, HttpServer, HttpResponse, Responder};
+use serde::{serialize, Deserialize};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use uuid::Uuid;
+
+/*
+    defining core types
+    */
+    
+#[derive(serialize, Deserialize, Clone)]
+struct Beacon {
+    id: Uuid,
+    hostname: String,
+    last_seen: String,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Task {
+    command: String,
+}
+
+#[derive(Serialize, Deseriaize, Clone)]
+struct ResultData{
+    output: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
