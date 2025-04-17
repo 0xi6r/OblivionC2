86af702b587c1326a0fe34467e1b7abaef58c468
issachar5 <193503195+issachar5@users.noreply.github.com>
2025-04-17 15:34:43 +0000

    Thu, Apr 17, 2025, 6:34 PM +03:00

---
+++ src/handlers.rs
@@ -0,0 +1,10 @@
+// implant check in
+
+async fn beacon(
+    data: web::Data<AppState>,
+    beacon: web::Json<Beacon>
+) -> impl Responder {
+    let mut beacons = data.beacons.lock().unwrap();
+    beacons.insert(beacon.id, beacon.into_inner());
+    HttpResponse::Ok().body("Beacon received")
+}
